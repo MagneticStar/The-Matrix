9ad1e483237ffb26e31fa9b041f9bd99089b9e95
MagneticStar <62677833+MagneticStar@users.noreply.github.com>
2023-03-22 15:16:12 -0500

    More Canvas

--- canvas.java
+++ canvas.java
@@ -1,9 +1,8 @@
 import java.awt.*;  
-import javax.swing.JFrame;
 
 public class canvas extends Canvas{
         public void paint(Graphics g) {  
-            g.drawString("Hello",40,40);  
+            g.drawString("Bonjour",40,40);  
             setBackground(Color.WHITE);  
             g.fillRect(130, 30,100, 80);  
             g.drawOval(30,130,50, 60);  
@@ -12,13 +11,5 @@ public class canvas extends Canvas{
             g.drawArc(30, 200, 40,50,90,60);  
             g.fillArc(30, 130, 40,50,180,40);  
               
-        }  
-        public static void main(String[] args) {  
-            canvas m=new canvas();  
-            JFrame f=new JFrame();  
-            f.add(m);  
-            f.setSize(400,400);  
-            //f.setLayout(null);  
-            f.setVisible(true);  
         }  
 }
