d16402461f7bde6da518a7e45ebf015e9d957875
MagneticStar <62677833+MagneticStar@users.noreply.github.com>
2023-04-24 19:18:06 -0500

    Somewhat good changes to color

--- Main.java
+++ Main.java
@@ -45,7 +45,7 @@ public class Main {
 
         // Make tick wait
         try {
-            Thread.sleep(100);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- SimulationPanel.java
+++ SimulationPanel.java
@@ -27,14 +27,14 @@ public class SimulationPanel extends JPanel{
 
         for(Food f : Database.foodsList) {
             g.setColor(f.getColor());
-            g.fillRect(f.getPrintPos().x() - 5, f.getPrintPos().y() - 5, 10, 10);
+            g.fillRect(f.getPrintPos().x() - 2, f.getPrintPos().y() - 2, 4, 4);
         }
     }
     public void drawWater(Graphics g) {
 
         for(Water w : Database.watersList) {
             g.setColor(w.getColor());
-            g.fillRect(w.getPrintPos().x() - 5, w.getPrintPos().y() - 5, 10, 10);
+            g.fillRect(w.getPrintPos().x() - 2, w.getPrintPos().y() - 2, 4, 4);
         }
     }
 }
