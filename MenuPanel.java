7462a120759ee908d05f18d13b72f074d2dffc60
Jackson <36348184+toytwo@users.noreply.github.com>
2024-02-14 19:01:24 -0600

    Revert "Merge branch 'Jackson'"
    This reverts commit ab1b524d42cc91e0d075c782018f498ea38d42a6, reversing
    changes made to cd73b70094624b1dc6077decdd4228b04b7d9611.

--- MenuPanel.java
+++ MenuPanel.java
@@ -3,26 +3,23 @@ import java.awt.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.BitSet;
 import java.io.File;
 import java.io.IOException;
 
 public class MenuPanel extends JPanel{
     private JButton newFileButton;
     private JButton oldFileButton;
+    private JLabel logo;
     private JTextField newFileTextField;
-    private BufferedImage logo;
 
     public MenuPanel() {
         this.setLayout(new GridBagLayout());
         setBackground(Color.black);
         createLoadComponents();
         addComponents();
-        
-        try {                
-          logo = ImageIO.read(new File("Logo.png"));
-       } catch (IOException ex) {
-       }
     }
     
     public void createLoadComponents(){
@@ -32,6 +29,23 @@ public class MenuPanel extends JPanel{
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Load Old File
+                try {
+                FileInputStream file = new FileInputStream(newFileTextField.getText());
+                ObjectInputStream in = new ObjectInputStream(file);
+                
+                Main.loaded = (Database)((Database)in.readObject()).clone();
+                for (int i = 0; i < Main.loaded.creaturesList.length; i++) {
+                    Main.loaded.creaturesList[i] = new Creature((BitSet)in.readObject());
+                }
+                in.close();
+            } catch (Exception exception) {
+                System.out.println(exception);
+                // Create generation 0
+                for (int i = 0; i < Main.loaded.creaturesList.length; i++) {
+                    Main.loaded.creaturesList[i] = new Creature();
+                }
+            }
+                Main.loaded.visualPanel = Screens.simulationPanel;
                 Main.startThread();
         }});
 
@@ -42,7 +56,8 @@ public class MenuPanel extends JPanel{
             public void actionPerformed(ActionEvent e) {
                 String fileName = newFileTextField.getText();
                 // Create New File
-                Main.startSimulation();
+                Main.loaded.fileName = fileName;
+                Main.startThread();
         }});
     }
     public void addComponents(){
@@ -50,21 +65,31 @@ public class MenuPanel extends JPanel{
         newFileTextField = new JTextField(20);
         newFileTextField.setForeground(Color.black);
         newFileTextField.setBackground(Color.white);
+        try {         
+          ImageIcon logoIcon = (new ImageIcon(ImageIO.read(new File("Logo.png"))));
+          logo = new JLabel(logoIcon);
+        }catch (IOException ex) {}
+        
         
         // Add the components (order matters)
         GridBagConstraints c = new GridBagConstraints();
         c.anchor = GridBagConstraints.CENTER;
-        c.insets = new Insets(0, 0, 0, 10);
-        c.gridx = 2;
+        c.insets = new Insets(0, 0, 0, -350);
+        c.gridx = 0;
         c.gridy = 1;
         this.add(newFileTextField,c);
-        c.insets = new Insets(0, 10, 0, 10);
+        c.insets = new Insets(0, 0, 0, 300);
         c.gridx = 0;
         c.gridy = 1;
         this.add(oldFileButton,c);
-        c.gridx = 1;
+        c.insets = new Insets(0, 0, 0, 0);
+        c.gridx = 0;
         c.gridy = 1;
         this.add(newFileButton,c);
+        c.anchor = GridBagConstraints.PAGE_START;
+        c.gridx = 0;
+        c.gridy = 0;
+        this.add(logo,c);
         
         this.revalidate();
     }
@@ -73,6 +98,5 @@ public class MenuPanel extends JPanel{
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         // Where all graphics are rendered
-        g.drawImage(logo, 0, 0, this);
     }
 }
